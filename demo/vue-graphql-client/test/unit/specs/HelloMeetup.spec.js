0c5cd6aed2e7a32ad6cc949339f660abeaed7771
Brendon Thiede <ThiedeB@courts.mi.gov>
2018-02-20 01:00:16 -0500

    Name change and smarter ignorance ;-P

--- demo/vue-graphql-client/test/unit/specs/HelloMeetup.spec.js
+++ demo/vue-graphql-client/test/unit/specs/HelloMeetup.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import HelloMeetup from '@/components/HelloMeetup'
 
-describe('HelloWorld.vue', () => {
+describe('HelloMeetup.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(HelloMeetup)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+      .toEqual('This way to the demo:')
   })
 })
