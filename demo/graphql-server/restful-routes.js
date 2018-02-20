406abd889652656a2e1b5ce8ec6b0a5b2e7de267
Brendon Thiede <ThiedeB@courts.mi.gov>
2018-02-19 23:33:16 -0500

    Updating REST endpoints

--- demo/graphql-server/restful-routes.js
+++ demo/graphql-server/restful-routes.js
@@ -1,51 +1,50 @@
 const people = require('./data/people.json')
+const species = require('./data/species.json')
+const homeworld = require('./data/homeworlds.json')
 const peopleBasics = require('./data/peopleBasics.json')
 const peopleDetails = require('./data/peopleDetails.json')
+const _ = require('lodash')
 
 module.exports = {
     "init": function (router) {
-        router.get('/', function(req, res) {
+        router.get('/', function (req, res) {
             res.json({
                 fatJson: '/api/people',
                 thinJson1: '/api/peopleBasics',
                 thinJson2: '/api/peopleDetails'
-            });   
+            });
         });
 
-        router.get('/people', function(req, res) {
+        router.get('/people', function (req, res) {
             res.json(people);
         });
 
-        router.get('/people/:id', function(req, res) {
-            const id = parseInt(req.params.id);
-
-            if (isNaN(id)) {
-                res.json(people.filter(person => (new RegExp(req.params.id, 'i')).test(person.name)));
-            } else {
-                res.json(people.filter(person => person.id === id));
-            }
+        router.get('/people/:id', function (req, res) {
+            res.json(_.find(people, { id: parseInt(req.params.id) }))
         });
 
-        router.get('/peopleBasics', function(req, res) {
+        router.get('/species/:id', function (req, res) {
+            res.json(_.find(species, { id: parseInt(req.params.id) }))
+        });
+
+        router.get('/homeworld/:id', function (req, res) {
+            res.json(_.find(homeworld, { id: parseInt(req.params.id) }))
+        });
+
+        router.get('/peopleBasics', function (req, res) {
             res.json(peopleBasics);
         });
 
-        router.get('/peopleBasics/:id', function(req, res) {
-            const id = parseInt(req.params.id);
-
-            if (isNaN(id)) {
-                res.json(peopleBasics.filter(person => (new RegExp(req.params.id, 'i')).test(person.name)));
-            } else {
-                res.json(peopleBasics.filter(person => person.id === id));
-            }
+        router.get('/peopleBasics/:id', function (req, res) {
+            res.json(_.find(peopleBasics, { id: parseInt(req.params.id) }))
         });
 
-        router.get('/peopleDetails', function(req, res) {
+        router.get('/peopleDetails', function (req, res) {
             res.json(peopleDetails);
         });
 
-        router.get('/peopleDetails/:id', function(req, res) {
-            res.json(peopleDetails.filter(person => person.id === parseInt(req.params.id)));
+        router.get('/peopleDetails/:id', function (req, res) {
+            res.json(_.find(peopleDetails, { id: parseInt(req.params.id) }))
         });
     }
 }
